d4f2dfba3f8132ba42bec3ac9d8e8ac0ce40fd6e
Felipe-Boiadeiro <felipeboiadeiro151007@gmail.com>
2026-04-15 16:51:30 -0300

    Update Q02.go

--- lista03/Q02.go
+++ lista03/Q02.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var soma, media int
+	soma, media, contador := 0, .0, 0
 	for x := 50; x <= 70; x += 2 {
 		soma += x
+		contador++
 	}
-	media = soma / 11
-	fmt.Println(media)
+	media = float64(soma) / float64(contador)
+
+	fmt.Printf("A soma é: %v\n A média é: %f", soma, media)
+
 }
